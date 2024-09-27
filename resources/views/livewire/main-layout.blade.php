1675d0c60c80420518d2a9024507babf8e7d1949
DivyaBandari-S <bandaridivya1@gmail.com>
2024-09-27 13:42:49 +0530

    new changes

--- resources/views/livewire/main-layout.blade.php
+++ resources/views/livewire/main-layout.blade.php
@@ -157,7 +157,7 @@
 			<li>
 				<a href="#"><i class="ph-gear-fill"></i></a>
 			</li>
-			<li>
+			<li wire:click="handleLogout">
 				<a href="#" class="logout"><i class="ph-sign-out-fill"></i></a>
 			</li>
 		</ul>
@@ -331,5 +331,31 @@
 			</ul>
 		</div>
 		<!-- end: MAIN TOP -->
+
+
+
+		<!-- Logout Modal -->
+		@if ($showLogoutModal)
+		<div class="modal" id="logoutModal" tabindex="-1" style="display: block;">
+			<div class="modal-dialog modal-dialog-centered">
+				<div class="modal-content">
+					<div class="modal-header text-white" style=" background-color: var(--main-button-color);">
+						<h6 class="modal-title " id="logoutModalLabel" style="align-items: center;">Confirm
+							Logout</h6>
+					</div>
+					<div class="modal-body text-center"
+						style="font-size: 14px;color:var( --main-heading-color);">
+						Are you sure you want to logout?
+					</div>
+					<div class="d-flex gap-3 justify-content-center p-3">
+						<button type="button" class="submit-btn mr-3"
+							wire:click="confirmLogout">Logout</button>
+						<button type="button" class="cancel-btn" wire:click="cancelLogout">Cancel</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="modal-backdrop fade show"></div>
+		@endif
 	</section>
 </div>
