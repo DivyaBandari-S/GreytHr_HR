da52bc839293a1d98dfb3b77a2e99831559cba9f
DivyaBandari-S <bandaridivya1@gmail.com>
2025-01-31 12:20:00 +0530

    main changes

--- routes/web.php
+++ routes/web.php
@@ -85,10 +85,10 @@ Route::get('/file/{id}', function ($id) {
 
 Route::middleware(['auth:hr', 'handleSession'])->group(function () {
 
-    Route::get('/', HomeDashboard::class)->name('home');
-    Route::get('/request', Requests::class)->name('request');
-    // Group routes under the 'hr' prefix
-    Route::prefix('hr')->group(function () {
+        Route::get('/', HomeDashboard::class)->name('home');
+        Route::get('/request', Requests::class)->name('request');
+        // Group routes under the 'hr' prefix
+        Route::prefix('hr')->group(function () {
 
 
         //home page routes
@@ -97,14 +97,13 @@ Route::middleware(['auth:hr', 'handleSession'])->group(function () {
         Route::get('/resig-requests', Resignationrequests::class)->name('resig-requests');
         Route::get('/HelpDesk', HelpDesk::class)->name('HelpDesk');
 
-
         //HR Employee-Main Submodule Routes
         Route::get('/user/main-overview', HrMainOverview::class)->name('main-overview');
         Route::get('/user/analytics-hub', AnalyticsHub::class)->name('analytics-hub');
         Route::get('/user/analytics-hub-viewall', AnalyticsHubViewAll::class)->name('analytics-hub-viewall');
         Route::get('/user/hremployeedirectory', EmployeeDirectory::class)->name('employee-directory');
-        Route::get('/user/create-attendance-exception', CreateAttendanceExceptionPage::class)->name('create-attendance-exception');
-        Route::get('/user/shift-rotation-calendar', ShiftRotationCalendar::class)->name('shift-rotation-calendar');
+        Route::get('/user/hr-organisation-chart', HrOrganisationChart::class)->name('hr-organisation-chart');
+
         //HR Employee-Information Submodule Routes
         Route::get('/hrFeeds', Feeds::class)->name('hrfeeds');
         Route::get('/everyone', Everyone::class)->name('everyone');
@@ -114,50 +113,49 @@ Route::middleware(['auth:hr', 'handleSession'])->group(function () {
         Route::get('parent-details', ParentDetails::class)->name('parent-details');
         Route::get('/emp-document', EmpDocument::class)->name('emp-document');
         Route::get('/bank-account', BankAccount::class)->name('bank-account');
-        Route::get('/user/attendance-process', AttendanceProcess::class)->name('attendance-process');
-        Route::get('/user/swipe-management-for-hr', SwipeManagementForHr::class)->name('swipe-management-for-hr');
-        Route::get('/user/employee-swipes-for-hr', EmployeeSwipesForHr::class)->name('employee-swipes-for-hr');
-        //HR Leave-Main Submodule Routes
-        Route::get('/user/hr-organisation-chart', HrOrganisationChart::class)->name('hr-organisation-chart');
-        Route::get('/user/employee-weekday-chart', EmployeeWeekDayChart::class)->name('employee-weekday-chart');
-        Route::get('/user/hr-attendance-overview', HrAttendanceOverviewNew::class)->name('attendance-overview');
-        Route::get('/user/who-is-in-chart-hr', WhoIsInChartHr::class)->name('who-is-in-chart-hr');
-        Route::get('/user/edit-attendance-exception-page/{id}', EditAttendanceExceptionPage::class)->name('edit-attendance-exception-page');
-        Route::get('/user/edit-shift-override/{id}', EditShiftOverride::class)->name('edit-shift-override');
-        Route::get('/user/shift-override', ShiftOverrideHr::class)->name('shift-override');
-        Route::get('/user/attendance-info', HrAttendanceInfo::class)->name('attendance-info');
-        Route::get('/review-pending-regularisation-for-hr/{id}/{emp_id}', RegularisationPendingForHr::class)->name('review-pending-regularisation-for-hr');
-        //HR Leave-Infomation Submodule Routes
-        Route::get('/user/employee-leave', EmployeeLeave::class)->name('employee-leave');
         Route::get('/user/employee-salary', EmployeeSalary::class)->name('employee-salary');
 
-
-        //HR Leave Related Routes
-        Route::get('/user/attendance-exception', AttendanceExceptionForDisplay::class)->name(name: 'attendance-exception');
         //HR Leave-Main Submodule Routes
         Route::get('/user/leave-overview', HrLeaveOverview::class)->name('leave-overview');
         Route::get('/user/leave-overview/{month}/{leaveType?}', HrLeaveOverview::class)->name('leave-overview.month');
         Route::get('/leave-overview/{monthLeaveType?}', HrLeaveOverview::class)->name('leave-overview.monthLeaveType');
+        Route::get('/user/hr-attendance-overview', HrAttendanceOverviewNew::class)->name('attendance-overview');
         Route::get('/user/leave-calendar', HrLeaveCalendar::class)->name('Leave-calendar');
+        Route::get('/user/who-is-in-chart-hr', WhoIsInChartHr::class)->name('who-is-in-chart-hr');
+
+        //HR Leave-Infomation Submodule Routes
+        Route::get('/user/employee-leave', EmployeeLeave::class)->name('employee-leave');
+        Route::get('/user/shift-roster-hr', ShiftRosterHr::class)->name(name: 'shift-roster-hr');
         Route::get('/user/attendance-info', HrAttendanceInfo::class)->name('attendance-info');
         Route::get('/user/attendance-muster-hr', AttendanceMusterHr::class)->name(name: 'attendance-muster-hr');
-        Route::get('/user/shift-roster-hr', ShiftRosterHr::class)->name(name: 'shift-roster-hr');
-        Route::get('/user/create-shift-override', CreateShiftOverride::class)->name(name: 'create-shift-override');
-
+        Route::get('/user/swipe-management-for-hr', SwipeManagementForHr::class)->name('swipe-management-for-hr');
+        Route::get('/user/employee-swipes-for-hr', EmployeeSwipesForHr::class)->name('employee-swipes-for-hr');
 
         //HR Leave-Admin Submodule Routes
         Route::get('/user/grantLeave', GrantLeaveBalance::class)->name('grantLeave');
         Route::get('/user/grant-summary', EmpLeaveGranterDetails::class)->name('grant-summary');
         Route::get('/user/leavePolicySettings', LeaveSettingPolicy::class)->name('leavePolicySettings');
         Route::get('/user/leaveYearEndProcess', YearEndProcess::class)->name('year-end-process');
+        Route::get('/user/attendance-exception', AttendanceExceptionForDisplay::class)->name(name: 'attendance-exception');
+        Route::get('/user/create-attendance-exception', CreateAttendanceExceptionPage::class)->name('create-attendance-exception');
+        Route::get('/user/edit-attendance-exception-page/{id}', EditAttendanceExceptionPage::class)->name('edit-attendance-exception-page');
+        Route::get('/user/create-shift-override', CreateShiftOverride::class)->name(name: 'create-shift-override');
+        Route::get('/user/edit-shift-override/{id}', EditShiftOverride::class)->name('edit-shift-override');
+        Route::get('/user/shift-override', ShiftOverrideHr::class)->name('shift-override');
         Route::get('/user/leave/admin/leaveRecalculator', LeaveRecalculator::class)->name('leave-recalculator');
+        Route::get('/user/attendance-process', AttendanceProcess::class)->name('attendance-process');
 
         //HR Leave-SetUp Submodule Routes
         Route::get('/user/holidayList', HrHolidayList::class)->name('holidayList');
         Route::get('/user/leave/setup/leave-type-reviewer', LeaveTypeReviewer::class)->name('leave-type-reviewer');
+        Route::get('/user/employee-weekday-chart', EmployeeWeekDayChart::class)->name('employee-weekday-chart');
+        Route::get('/user/shift-rotation-calendar', ShiftRotationCalendar::class)->name('shift-rotation-calendar');
+
+        //extra routes
+        Route::get('/review-pending-regularisation-for-hr/{id}/{emp_id}', RegularisationPendingForHr::class)->name('review-pending-regularisation-for-hr');
+
         //Reports
         Route::get('/user/reports/', ReportsManagement::class)->name('reports');
-
     });
 });
 
@@ -235,6 +233,7 @@ Route::get('/encode-decode/{value}', function ($value) {
         return response()->json(['action' => 'encrypted', 'value' => $encrypted]);
     }
 });
+
 use Illuminate\Support\Facades\Hash;
 
 Route::get('/hash-verify/{value}', function ($value) {
