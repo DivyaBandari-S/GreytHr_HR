a29b2b53220e728088000e767c915ed03e1cea82
DivyaBandari-S <bandaridivya1@gmail.com>
2025-03-10 17:15:43 +0530

    main changes

--- app/Livewire/PayrollArrears.php
+++ app/Livewire/PayrollArrears.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class PayrollArrears extends Component
 {
+    public $currentStep = 1;
+    public $showPayArrearsSection  = false;
+    public $showOverViewSection = true;
+
+    public function toggleSection()
+    {
+        $this->showPayArrearsSection  = !$this->showPayArrearsSection;
+        $this->showOverViewSection = !$this->showOverViewSection;
+    }
+    public function nextStep()
+    {
+
+        if ($this->currentStep < 3) {
+            $this->currentStep++;
+        }
+    }
+
+    public function gotoBack()
+    {
+        if ($this->currentStep) {
+            $this->currentStep--;
+        }
+    }
     public function render()
     {
         return view('livewire.payroll-arrears');
