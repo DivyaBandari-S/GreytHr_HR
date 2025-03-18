f5272f78fe31702508aff7931a70535839f87b1d
archanagoud28 <125112613+archanagoud28@users.noreply.github.com>
2025-03-18 12:17:17 +0530

    mychanges

--- resources/views/livewire/income-tax.blade.php
+++ resources/views/livewire/income-tax.blade.php
@@ -141,21 +141,7 @@
 
 
     @endif
-    <div class=" align-items-end">
-    <div class="row" style="display: flex; align-items: flex-end;width:100%">
-    <div style="width:80%" >
-        <p style="font-size: 10px; color: grey;">INCOME TAX PROCESSED ON</p>
-    </div>
-   
-</div>
 
-<div class="row" style="display: flex; align-items: flex-end;">
-
-    <div class="column">
-        <p style="font-size: 10px; color: grey;">NEW TAX REGIME</p>
-    </div>
-</div>
-    </div>
 
 
 
@@ -165,7 +151,7 @@
     </div>
     </div>
     <div class="container mt-4">
-    <ul class="nav nav-tabs" id="myTabs">
+    <ul class="nav nav-tabs" id="myTabs" style="font-size: 10px;">
         <li class="nav-item">
             <a class="nav-link active" data-bs-toggle="tab" href="#income">Income</a>
         </li>
@@ -190,14 +176,13 @@
         <li class="nav-item">
             <a class="nav-link" data-bs-toggle="tab" href="#regime">Regime</a>
         </li>
-        <li class="nav-item">
-            <a class="nav-link" data-bs-toggle="tab" href="#result">Result</a>
-        </li>
+  
     </ul>
 
     <div class="tab-content mt-3">
-        <div class="tab-pane fade show active" id="income">
-            <h4>Income</h4>
+        <div class="tab-pane fade show active m1-5" id="income">
+            <h4>Tax Related Components
+            </h4>
             <p>Content for Income tab.</p>
         </div>
         <div class="tab-pane fade" id="prev-income">
@@ -228,10 +213,7 @@
             <h4>Regime</h4>
             <p>Content for Regime tab.</p>
         </div>
-        <div class="tab-pane fade" id="result">
-            <h4>Result</h4>
-            <p>Content for Result tab.</p>
-        </div>
+       
     </div>
 </div>
 
